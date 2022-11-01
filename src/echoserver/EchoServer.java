4358b849eccf9269182522422709fb0a8d15823a
Dongting Cai <cai00151@morris.umn.edu>
2022-11-01 16:11:32 -0500

    Update `EchoServer`, base code setup
    < Issue #2 >
    Update `EchoServer.java`, Setup the base code

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,14 +1,10 @@
 package echoserver;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
 public class EchoServer {
 	
-	// REPLACE WITH PORT PROVIDED BY THE INSTRUCTOR
 	public static final int PORT_NUMBER = 0; 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		EchoServer server = new EchoServer();
@@ -17,15 +13,12 @@ public class EchoServer {
 
 	private void start() throws IOException, InterruptedException {
 		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
+
 		while (true) {
 			Socket socket = serverSocket.accept();
 
-			// Put your code here.
-			// This should do very little, essentially:
-			//   * Construct an instance of your runnable class
-			//   * Construct a Thread with your runnable
-			//      * Or use a thread pool
-			//   * Start that thread
+			ServerThread serverThread = new ServerThread(socket);
+			serverThread.start();
 		}
 	}
 }
